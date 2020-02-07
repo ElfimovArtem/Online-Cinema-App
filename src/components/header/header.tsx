b10bade228ec390fa2eda2069abdf7365c12a5b4
ElfimovArtem <elfimov021@yandex.ru>
2020-02-07 10:25:11 +0300

    Implemented the movie’s internal page.

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,14 +1,17 @@
 import * as React from 'react';
 import cn from 'classnames';
+import defaultHeaderImage from '../../assets/header-background.jpg';
 import './header-styles.scss';
 
 interface HeaderPropTypes {
   title: string;
   description: string;
+  imgURL?: string;
   fullSize?: boolean;
 }
 export const Header = (props: HeaderPropTypes) => (
   <div className={cn('header', { 'header--fullsize': props.fullSize })}>
+    <img className={cn('header__background')} src={props.imgURL || defaultHeaderImage} alt={props.title} />
     <div className={cn('header__shadow')} />
     <h1 className={cn('header__title')}>{props.title}</h1>
     <p className={cn('header__description')}>{props.description}</p>
--- src/pages/movie-page/movie-page.tsx
+++ src/pages/movie-page/movie-page.tsx
@@ -1,3 +1,35 @@
 import * as React from 'react';
+import cn from 'classnames';
+import { Redirect } from 'react-router-dom';
+import { Header } from '../../components/header';
+import { Footer } from '../../components/footer';
+import { MovieElement } from '../../modules/movies';
+import './movie-page-styles.scss';
 
-export const MoviePage = () => <div>Movie Page</div>;
+export interface MoviesPagePropTypes {
+  currentMovie: MovieElement | undefined,
+}
+
+export const MoviePage = ({ currentMovie }: MoviesPagePropTypes) => (
+  currentMovie ? (
+    <div>
+      <Header
+        imgURL={currentMovie.images[0]}
+        title={currentMovie.name}
+        description={currentMovie.tags.join(', ')}
+        fullSize
+      />
+      <div className={cn('movie-page-wrapper')}>
+        <h2 className={cn('movie-page-wrapper__title')}>
+          Описание
+        </h2>
+        <p className={cn('movie-page-wrapper__description')}>
+          {currentMovie.description}
+        </p>
+      </div>
+      <Footer
+        description="© 2019 ООО «onlinecinema.ru»"
+      />
+    </div>
+  ) : (<Redirect to="/" />)
+);
